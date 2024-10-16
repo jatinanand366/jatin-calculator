8a2f9f3441cc2ef6bb1835d936609b0fcbcb4c45
jatinanand366 <jatintechieanand@gmail.com>
2024-10-16 14:05:15 +0530

    TFMS-1 : Initiate with normal independent method.

---
+++ index.js
@@ -0,0 +1,7 @@
+function addNew(a, b) {
+    console.log(a, b, a + b);
+    return a + b;
+}
+module.exports = {
+    addNew
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+const { addNew } = require('./index');
+
+console.log(addNew(3,4));
